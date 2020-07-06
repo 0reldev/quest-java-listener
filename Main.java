27accdf84785caf3ca197556f3a0ef5111e60213
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-07-06 11:55:15 +0200

    Quest solved.

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
+import javax.xml.crypto.dsig.TransformService;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Bank account = new Bank(1000);
 
-        // TODO : create and use a TransactionListener interface with onComplete method
-        Transactions.withdraw(account, 100);
+        Transactions.withdraw(account, 100, new TransactionListener() {
+            @Override
+            public void onComplete(Bank account) {
+                System.out.printf("Your balance is: %d%n", account.getTotal());
+            }
+        });
 
-        System.out.printf("Your balance is: %d%n", account.getTotal());
+
     }
 }
